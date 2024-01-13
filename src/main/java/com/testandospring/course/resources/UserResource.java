b14d217b5370ccae2a46ae28e806f4fb4d4abf09
Deyvid Yancey <deyvid12q@gmail.com>
2024-01-13 16:28:04 -0300

    User entitiy and resource

---
+++ src/main/java/com/testandospring/course/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.testandospring.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.testandospring.course.entities.User;
+
+@RestController
+@RequestMapping(value= "/users")
+public class UserResource {
+
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+	User u = new User(1L, "Maria", "maria@gmaol.com", "999999", "12345");
+	return ResponseEntity.ok().body(u);
+	
+}
+}
